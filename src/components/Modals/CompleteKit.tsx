202864e58bf2d4b3caf6d70f5d82bcfb7f21739f
alexbilakh <oskar.kamil121@gmail.com>
2024-09-02 06:20:06 +0500

    fixing 2

--- src/components/Modals/CompleteKit.tsx
+++ src/components/Modals/CompleteKit.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import ModalLayout from "./ModalLayout";
-import { Button } from "@mui/material";
+import DotShadowRoundedContainer from "../DotShadow";
+import { Button, OutlinedInput } from "@mui/material";
 import { useKitContext } from "@/providers/KitProvider";
 
 interface CompleteKitModalProps {
@@ -9,6 +11,7 @@ interface CompleteKitModalProps {
 
 const CompleteKitModal = (props: CompleteKitModalProps) => {
   const { visible, onClose } = props;
+
   const { completeKit } = useKitContext();
 
   const onOkClicked = () => {
@@ -17,24 +20,35 @@ const CompleteKitModal = (props: CompleteKitModalProps) => {
   }
 
   return (
-    <ModalLayout visible={visible} onClose={onClose}>
-      <div className="flex flex-col bg-white p-[40px] gap-[20px] text-center max-w-[400px]">
-        <div className="text-[18px] font-bold">
-          {`Congratulations!`}
-        </div>
+    <ModalLayout visible={visible} onClose={onClose} showCloseIcon={false}>
+      <div className="flex bg-white px-[40px] py-[30px] gap-[48px] items-center rounded-[20px] max-w-[750px]">
+        <Image
+          src={`/images/characters/suggest-kit.png`}
+          alt="character"
+          width={255}
+          height={279}
+          sizes="100vw"
+        />
 
-        <div>
-          {`You completed your kit and you eraned this many points. Please reach out to your parenet so they can approve your points which will get added to your account.`}
-        </div>
-        <div>
-          <Button
-            variant="contained"
-            className="!bg-my-orange !rounded-full"
-            onClick={onOkClicked}
-          >
-            <span className="font-cathy-melody"> {"Ok"}</span>
-          </Button>
-        </div>
+        <DotShadowRoundedContainer activeColor="#F4961D" animate={false}>
+          <div className="p-[24px] flex flex-col gap-[12px] bg-[#FEF4E8]">
+            <div className="text-my-blue font-cathy-melody text-[20px]">
+              {"Congratulations you completed your kit and you earned this many points!"}
+            </div>
+
+            <div>{`Please reach out to your pareent so they can approve your points which will get added to your account`}</div>
+
+            <div className="flex items-center justify-end">
+              <Button
+                variant="contained"
+                className="!bg-my-orange !rounded-full"
+                onClick={onOkClicked}
+              >
+                <span className="font-cathy-melody"> {"Ok"}</span>
+              </Button>
+            </div>
+          </div>
+        </DotShadowRoundedContainer>
       </div>
     </ModalLayout>
   );
